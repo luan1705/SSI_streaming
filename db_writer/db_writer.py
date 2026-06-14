2e4798f2a03633aceb9cf34328c77019e39a4355
luan1705 <phamquangluan17@gmail.com>
2026-06-15 02:25:27 +0700

    trước khi đổi từ videv.cloud thành tanhungsoft.com

--- db_writer/db_writer.py
+++ db_writer/db_writer.py
@@ -16,7 +16,7 @@ logging.basicConfig(level=logging.INFO, format="%(asctime)s %(levelname)s %(mess
 REDIS_URL = os.getenv("REDIS_URL", "redis://default:%40Vns123456@videv.cloud:6379/1")
 DB_URL    = os.getenv("DB_URL", "postgresql+psycopg2://vnsfintech:Vns_123456@videv.cloud:5433/vnsfintech")
 
-PATTERNS = os.getenv(
+PATTERNS = os.getenv( 
     "PATTERNS",
     "asset,active,indices"
 ).split(",")
